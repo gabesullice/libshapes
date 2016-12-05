b47ffa10aadc9edf40dcb1b83b1f32ed642d7988
Gabriel SULLICE <gabriel@sullice.com>
2016-12-05 16:28:06 -0700

    Start implementation of Composition class

--- src/Composition.js
+++ src/Composition.js
@@ -1,7 +1,36 @@
-export class Composition {
+export default class Composition {
 
-  constructor(shapes) {
-    this._shapes = shapes;
+  constructor(options) {
+    this._bounds = options === undefined ? [100,100] : options.bounds;
+    this._count = 0;
+    this._figures = {};
+  }
+
+  figures() {
+    return this._figures;
+  }
+
+  bounds(values) {
+    if (values !== undefined) {
+      this._bounds = values;
+    }
+    return this._bounds;
+  }
+
+  add(figure) {
+    const id = this._getID();
+    this._figures[id] = figure;
+    return id;
+  }
+
+  remove(id) {
+    return delete this._figures[id];
+  }
+
+  _getID() {
+    const id = "fig-" + this._count;
+    this._count++;
+    return id;
   }
 
 }
--- tests/Composition.js
+++ tests/Composition.js
@@ -1,19 +1,62 @@
 import test from "ava";
-import Shape from "../lib/Shape.js";
-import * as composition from "../lib/Composition";
+import Shape from "../lib/Shape";
+import Figure from "../lib/Figure";
+import Composition from "../lib/Composition";
 
-test("Can create a new composition of shapes", t => {
-  const tests = [
-    {values: []},
-    {values: [new Shape([[0,0], [0,1], [1,0]])]}, // right triangle
-    {values: [new Shape([[0,0], [0,1], [1,1], [1,0]]), new Shape([[0,0], [0,1], [1,0]])]}, // square and right triangle
+test("Can instantiate a new composition", t => {
+  const c = new Composition();
+});
+
+test("Can set the bounds of a composition", t => {
+  const cases = [
+    {input: undefined, expected: [100,100]},
+    {input: [1000,1000], expected: [1000,1000]},
+    {input: [-100,-100], expected: [-100,-100]},
   ];
-  tests.forEach(test => {
-    const c = new composition.Composition(test.values);
-    t.deepEqual(c._shapes, test.values);
+  cases.forEach(item => {
+    const c = new Composition();
+    t.deepEqual(c.bounds(item.input), item.expected);
   });
 });
 
-//test("Can find overlapping shapes within a composition", t => {
-//
-//});
+test("Can add multiple figures to a composition", t => {
+  const figure = new Figure({shape: new Shape([[0,0], [0,1], [1,0]])});
+  const cases = [
+    {input: figure, expected: "fig-0"},
+    {input: figure, expected: "fig-1"},
+  ];
+  const c = new Composition();
+  cases.forEach(item => {
+    t.is(c.add(item.input), item.expected);
+  });
+});
+
+test("Can get all figures in a composition", t => {
+  const figure = new Figure({shape: new Shape([[0,0], [0,1], [1,0]])});
+  const cases = [
+    {input: [], expected: []},
+    {input: [figure], expected: ["fig-0"]},
+    {input: [figure, figure], expected: ["fig-0", "fig-1"]},
+  ];
+  cases.forEach(item => {
+    const c = new Composition();
+    item.input.forEach(fig => { c.add(fig)});
+    t.deepEqual(Object.keys(c.figures()), item.expected);
+  });
+});
+
+test("Can remove figures in a composition by ID", t => {
+  const figure = new Figure({shape: new Shape([[0,0], [0,1], [1,0]])});
+  const cases = [
+    {input: [], remove: [], expected: []},
+    {input: [figure], remove: ["fig-0"], expected: []},
+    {input: [figure, figure], remove: ["fig-0"], expected: ["fig-1"]},
+    {input: [figure, figure], remove: ["fig-2"], expected: ["fig-0", "fig-1"]},
+  ];
+  cases.forEach(item => {
+    const c = new Composition();
+    item.input.forEach(fig => { c.add(fig)});
+    item.remove.forEach(id => { c.remove(id)});
+    t.deepEqual(Object.keys(c.figures()), item.expected);
+  });
+});
