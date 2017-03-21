77e1adcd320a4badcdfa2719785d95a232cabbc2
Gabriel SULLICE <gabriel@sullice.com>
2017-03-21 14:45:57 -0600

    Dog face test

---
+++ tests/regression/default.js
@@ -0,0 +1,50 @@
+import * as composition from '../../src/Composition';
+import fs from 'fs';
+import test from 'ava';
+
+const DATA_DIRECTORY = 'tests/data';
+const PREFIXES = ['bug-report'];
+
+const histories = fs.readdirSync(DATA_DIRECTORY)
+  .filter(fileName => PREFIXES.some(prefix => fileName.startsWith(prefix)))
+  .map(fileName => {
+    return fs.readFileSync(`${DATA_DIRECTORY}/${fileName}`);
+  })
+  .map(data => {
+    return JSON.parse(data);
+  });
+
+histories.forEach((history, index) => {
+  const check = (history.failing) ? test.failing : test;
+
+  const ignore = new Set(history.input.ignore.concat(history.input.spaces));
+  const shouldKeep = function () {
+    return [...arguments].every(id => !ignore.has(id));
+  };
+
+  check(`Can validate a composition - Test #${index}`, t => {
+    const C = composition.fromHistory(history.input.history);
+    //console.log(printComposition(C));
+    const overlapping = C.overlapping().filter(pair => shouldKeep(pair.a, pair.b));
+    const nonCoincident = C.nonCoincident().filter(id => shouldKeep(id));
+    //console.log(overlapping);
+    t.is(overlapping.length, 0);
+    t.skip.is(nonCoincident.length, 0);
+  });
+});
+
+function printComposition(C) {
+  return Object.values(C.figures())
+    .map(F => printFigure(F))
+    .join('\n');
+}
+
+function printFigure(F) {
+  return F.vertices()
+    .map(V => printVertex(V))
+    .join('\n');
+}
+
+function printVertex(V) {
+   return [V.x, V.y].join(',');
+}
